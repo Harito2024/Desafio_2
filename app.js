9ba6a89aa77d618405d072433db6ac376f4f4a93
EzeDev280 <Ezequiel.Haro_Dev@hotmail.com>
2024-04-04 19:02:21 -0300

    unificacion de idioma

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const { futimes } = require('fs')
-const { isAsyncFunction } = require('util/types')
-
 const fs = require('fs').promises
 
 
@@ -10,19 +7,19 @@ class ProductManager {
 
     constructor(path) {
         this.products = [],
-            this.path = path
+        this.path = path
     }
 
     async addProduct(newObjet) {
 
-        let { title, descripcion, price, thumbnail, code, stock } = newObjet
+        let { title, description, price, thumbnail, code, stock } = newObjet
 
-        if (!title || !descripcion || !price || !thumbnail || !code || !stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log('Faltan campos')
             return
         }
-        const codeEncotrado = this.products.find((id) => id.code === code)
-        if (codeEncotrado) {
+        const codeFound = this.products.find((id) => id.code === code)
+        if (codeFound) {
             console.log('Codigo de Producto repetido')
             return
         }
@@ -30,7 +27,7 @@ class ProductManager {
         const newProduct = {
             id: ++ProductManager.lastId,
             title,
-            descripcion,
+            description,
             price,
             thumbnail,
             code,
@@ -41,7 +38,7 @@ class ProductManager {
         console.log(newProduct)
 
         /*Guardo el newObjet en el archivo*/
-        await this.guardarArchivo(this.products)
+        await this.saveFilesMio(this.products)
 
     }
     getProduct() {
@@ -50,23 +47,21 @@ class ProductManager {
     async getProductById(id) {
 
         try {
-            const arrayDeProductos = await this.leerArchivo()
-            const encontrado = arrayDeProductos.find(item => item.id === id)
-            if (!encontrado) {
+            const arrayProducts = await this.readFilesMio()
+            const idFound = arrayProducts.find(item => item.id === id)
+            if (!idFound) {
                 console.log('No se ecuentra el producto con ese id')
             } else {
-                console.log('Producto encotrado')
-                return encontrado
+                console.log('Producto encontrado')
+                return idFound
             }
         } catch (error) {
             console.log('Error en lectura de  archivo', error)
         }
-
-        
     }
 
     //--------------------Nuevos Metodos----------------//
-    async leerArchivo() {
+    async readFilesMio() {
         try {
             const res = await fs.readFile(this.path, 'utf-8')
             const arrayDeProductos = JSON.parse(res)
@@ -76,9 +71,9 @@ class ProductManager {
         }
     }
 
-    async guardarArchivo(arrayDeProductos) {
+    async saveFilesMio(arrayProducts) {
         try {
-            await fs.writeFile(this.path, JSON.stringify(arrayDeProductos, null, 2))
+            await fs.writeFile(this.path, JSON.stringify(arrayProducts, null, 2))
         } catch (error) {
             console.log('Error al guardar el archivo', error)
         }
@@ -86,12 +81,12 @@ class ProductManager {
 
     async upDateProduct(id, productUpdate){
         try {
-            const arrayProductos = await this.leerArchivo()
+            const arrayProducts = await this.readFilesMio()
 
-            const index = arrayProductos.findIndex(item => item.id === id)
+            const index = arrayProducts.findIndex(item => item.id === id)
             if(index !== -1){
-                arrayProductos.splice(index, 1, productUpdate)
-                await this.guardarArchivo(arrayProductos)
+                arrayProducts.splice(index, 1, productUpdate)
+                await this.saveFilesMio(arrayProducts)
             }else{
                 console.log('NO se encontro el producto a actualizar')
             }
@@ -103,9 +98,9 @@ class ProductManager {
 
     async deleteProduct(id){
         try {
-            const arrayProductos = await this.leerArchivo()
-            const borrarActualizar = arrayProductos.filter(item => item.id != id)
-            await this.guardarArchivo(borrarActualizar)
+            const arrayProducts = await this.readFilesMio()
+            const deleteUpDate = arrayProducts.filter(item => item.id != id)
+            await this.saveFilesMio(deleteUpDate)
 
         } catch (error) {
             console.log('No se puede pudo borrar el producto')
@@ -115,56 +110,56 @@ class ProductManager {
 
 //Test
 const manager = new ProductManager('./productos.json')
-//manager.getProduct()
+manager.getProduct()
 
 
 const product1 = {
 
     title: 'Produto Prueba1',
-    descripcion: 'Este es un Producto de Prueba1',
+    description: 'Este es un Producto de Prueba1',
     price: 200,
     thumbnail: 'Sin Imagen',
     code: 'abc123',
     stock: 25
 }
-//manager.addProduct(product1)
+manager.addProduct(product1)
 
 const product2 = {
     title: 'Produto Prueba2',
-    descripcion: 'Este es un Producto de Prueba2',
+    description: 'Este es un Producto de Prueba2',
     price: 300,
     thumbnail: 'Sin Imagen',
     code: 'abc124',
     stock: 35
 }
-//manager.addProduct(product2)
+manager.addProduct(product2)
 
 const product3 = {
     title: 'Produto Prueba3',
-    descripcion: 'Este es un Producto de Prueba 3 con error de campo al cual le falta el precio',
+    description: 'Este es un Producto de Prueba 3 con error de campo al cual le falta el precio',
     //price: 300,
     thumbnail: 'Sin Imagen',
     code: 'abc124',
     stock: 35
 }
-//manager.addProduct(product3)
+manager.addProduct(product3)
 
 const product4 = {
     title: 'Produto Prueba4',
-    descripcion: 'Este es un Producto de Prueba4 con codigo repetido',
+    description: 'Este es un Producto de Prueba4 con codigo repetido',
     price: 300,
     thumbnail: 'Sin Imagen',
     code: 'abc124',
     stock: 35
 }
-//manager.addProduct(product4)
+manager.addProduct(product4)
 
 async function testBuscadoPorId(id){
     const encontrado = await manager.getProductById(id)
     console.log(encontrado)
 }
 
-//testBuscadoPorId(3)
+testBuscadoPorId(2)
 
 const product5 = {
     id: 1,
